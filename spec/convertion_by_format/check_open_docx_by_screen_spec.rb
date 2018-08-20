ba70529308e0bc34d49e201cfd52601cf595c873
Dmitriy Rotatyy <flamine@list.ru>
2018-08-20 11:11:35 +0300

    add pending https://bugzilla.onlyoffice.com/show_bug.cgi?id=32793

--- spec/convertion_by_format/check_open_docx_by_screen_spec.rb
+++ spec/convertion_by_format/check_open_docx_by_screen_spec.rb
@@ -11,6 +11,7 @@ describe 'Convert docx files by convert service' do
       pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=38420' if file_path == 'docx/Office Open XML Part 1 - Fundamentals.docx'
       pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=38432' if file_path == 'docx/Диплом (Илья).docx'
       pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=38434' if file_path == 'docx/НАЧАЛАМГ 21-05-02.docx'
+      pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=32793' if file_path == 'docx/Office Open XML Part 4 - Markup Language Reference.docx'
       link = s3.get_object(file_path).presigned_url(:get, expires_in: 3600).split('?X-Amz-Algorithm')[0]
       response = converter.perform_convert(url: link, outputtype: 'png')
       expect(response[:url].nil?).to be_falsey
