a24b36c3a38326b98f3c2a7aa5e8ab17768e177d
Dmitriy Rotatyy <flamine@list.ru>
2018-09-27 14:35:02 +0300

    Add pending to files 50000strings.xlsx and 70000strings.xlsx. Timeout error

--- spec/convertion_by_format/check_open_xlsx_by_screen_spec.rb
+++ spec/convertion_by_format/check_open_xlsx_by_screen_spec.rb
@@ -14,6 +14,8 @@ describe 'Convert docx files by convert service' do
       pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=38488' if file_path == 'xlsx/Smaller50MB.xlsx'
       pending 'Timeout error' if file_path == 'xlsx/Mo drinks.xlsx'
       pending 'Timeout error' if file_path == 'xlsx/MODELO_planilhaControleFinanceiro.xlsx'
+      pending 'Timeout error' if file_path == 'xlsx/70000strings.xlsx'
+      pending 'Timeout error' if file_path == 'xlsx/50000strings.xlsx'
       link = s3.get_object(file_path).presigned_url(:get, expires_in: 3600).split('?X-Amz-Algorithm')[0]
       response = converter.perform_convert(url: link, outputtype: 'png')
       expect(response[:url].nil?).to be_falsey
