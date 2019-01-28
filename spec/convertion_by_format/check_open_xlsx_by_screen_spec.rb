3119d5720d6e7f9926f836e564b17a0906af0146
Dmitriy Rotatyy <flamine@list.ru>
2019-01-28 13:33:35 +0300

    Add timeout pendings to Hasil Treasure 2010 Season 2.xlsx file

--- spec/convertion_by_format/check_open_xlsx_by_screen_spec.rb
+++ spec/convertion_by_format/check_open_xlsx_by_screen_spec.rb
@@ -17,6 +17,7 @@ describe 'Convert docx files by convert service' do
       pending 'Timeout error' if file_path == 'xlsx/MODELO_planilhaControleFinanceiro.xlsx'
       pending 'Timeout error' if file_path == 'xlsx/70000strings.xlsx'
       pending 'Timeout error' if file_path == 'xlsx/50000strings.xlsx'
+      pending 'Timeout error' if file_path == 'xlsx/Hasil Treasure 2010 Season 2.xlsx'
       pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=39343' if file_path == 'xlsx/draw_00_ms2013.xlsx'
       pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=39491' if file_path == 'xlsx/Book 78.xlsx'
       link = s3.get_object(file_path).presigned_url(:get, expires_in: 3600).split('?X-Amz-Algorithm')[0]
