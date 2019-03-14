4b3c577781d27b6ea8df79ef453010ebbb5cada9
Dmitriy Rotatyy <flamine@list.ru>
2019-03-14 16:34:07 +0300

    Add pending for some tests. It is conversion error after conversing to pdf

--- spec/convertion_by_format/check_open_xlsx_by_screen_spec.rb
+++ spec/convertion_by_format/check_open_xlsx_by_screen_spec.rb
@@ -21,6 +21,17 @@ describe 'Convert docx files by convert service' do
       pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=39343' if file_path == 'xlsx/draw_00_ms2013.xlsx'
       pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=39491' if file_path == 'xlsx/Book 78.xlsx'
       pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=40862' if file_path == 'xlsx/2-.xlsx'
+      pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=40863' if file_path == 'xlsx/_1-3-4-.xlsx'
+      pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=40819' if file_path == 'xlsx/_1_2013_1.xlsx'
+      pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=40819' if file_path == 'xlsx/___01.02.2014.xlsx'
+      pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=40819' if file_path == 'xlsx/_---.xlsx'
+      pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=40819' if file_path == 'xlsx/7.xlsx'
+      pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=40819' if file_path == 'xlsx/Aanvalsplanner_v.2.2.xlsx'
+      pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=40819' if file_path == 'xlsx/Aanvalsplanner_v.2.2_©isaak.dieleman@gmail.com.xlsx'
+      pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=40819' if file_path == 'xlsx/Book 49.xlsx'
+      pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=40819' if file_path == 'xlsx/размещение в Барабу, схема .xlsx'
+      pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=40867' if file_path == 'xlsx/___--2016.xlsx'
+      pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=40863' if file_path == 'xlsx/_6_1 (2).xlsx'
       link = s3.get_object(file_path).presigned_url(:get, expires_in: 3600).split('?X-Amz-Algorithm')[0]
       response = converter.perform_convert(url: link, outputtype: 'png')
       expect(response[:url].nil?).to be_falsey
