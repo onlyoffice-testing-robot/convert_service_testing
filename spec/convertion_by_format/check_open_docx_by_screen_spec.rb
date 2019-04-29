65b88bd0d5d845ede6c2a1f5a1a86baa4c248ac0
Dmitriy Rotatyy <flamine@list.ru>
2019-04-29 17:11:54 +0300

    Delete pending because bug in invalid

--- spec/convertion_by_format/check_open_docx_by_screen_spec.rb
+++ spec/convertion_by_format/check_open_docx_by_screen_spec.rb
@@ -14,7 +14,6 @@ describe 'Convert docx files by convert service' do
       pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=32793' if file_path == 'docx/Office Open XML Part 4 - Markup Language Reference.docx'
       pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=40855' if file_path == 'docx/Adams S.E. - Molecular Similarity and Xenobiotic Metabolism.docx'
       pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=41481' if file_path == 'docx/Appendix+I_IPP.docx'
-      pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=41482' if file_path == 'docx/ManyOle.docx'
       link = s3.get_object(file_path).presigned_url(:get, expires_in: 3600).split('?X-Amz-Algorithm')[0]
       response = converter.perform_convert(url: link, outputtype: 'png')
       expect(response[:url].nil?).to be_falsey
