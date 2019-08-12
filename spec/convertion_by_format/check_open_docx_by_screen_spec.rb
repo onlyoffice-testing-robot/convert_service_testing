ba473b6d40fe63829b790839cde7e05b065a641d
Dmitriy Rotatyy <flamine@list.ru>
2019-08-12 12:34:45 +0300

    Add "pending" status for tests (#15)
    * Add pending 42324
    
    * Add pending 42327
    
    * Add pending 42334
    
    * Add pending 42340
    
    * Add pending 42343
    
    * Add pending 42345

--- spec/convertion_by_format/check_open_docx_by_screen_spec.rb
+++ spec/convertion_by_format/check_open_docx_by_screen_spec.rb
@@ -10,6 +10,7 @@ describe 'Convert docx files by convert service' do
     it File.basename(file_path) do
       pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=38299' if file_path == 'docx/ген_после_конвертирования_из_док.docx'
       pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=32793' if file_path == 'docx/Office Open XML Part 4 - Markup Language Reference.docx'
+      pending 'https://bugzilla.onlyoffice.com/show_bug.cgi?id=42324' if file_path == 'docx/SUMMER_APPLICATION_PG_1.docx'
       link = s3.get_object(file_path).presigned_url(:get, expires_in: 3600).split('?X-Amz-Algorithm')[0]
       response = converter.perform_convert(url: link, outputtype: 'png')
       expect(response[:url].nil?).to be_falsey
