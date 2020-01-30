49515aa4e63898e870d592afee054a2061afc9e1
Dmitriy Rotatyy <flamine@list.ru>
2020-01-30 18:19:48 +0300

    Add ability to use private s3 bucket for getting files for testing (#31)

--- config/StaticData.rb
+++ config/StaticData.rb
@@ -1,12 +1,21 @@
 # frozen_string_literal: true
 
 class StaticData
-  PROJECT_NAME = 'Convert Service Testing'
-  DOCUMENTSERVER = ENV['DOCUMENTSERVER'] || 'https://doc-linux.teamlab.info'
-  POSITIVE_STATUSES = %w[passed passed_2 pending].freeze
 
+  PROJECT_NAME = 'Convert Service Testing'
+  POSITIVE_STATUSES = %w[passed passed_2 pending].freeze
   PALLADIUM_SERVER = 'palladium.teamlab.info'
   JWT_ENABLE = ENV['USE_JWT'] != 'no'
+
+  TMP_FOLDER = 'files_tmp'
+
+  def self.nginx_url
+    ENV['NGINX'] || 'http://nginx'
+  end
+
+  def self.documentserver_url
+    ENV['DOCUMENTSERVER'] || 'http://documentserver'
+  end
 
   def self.get_jwt_key
     File.read("#{ENV['HOME']}/.documentserver/documentserver_jwt")
