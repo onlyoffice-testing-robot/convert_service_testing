7b4e83f708333cedcad580bc793026b916f65975
askonev <askonev007@gmail.com>
2020-05-27 16:08:24 +0300

    binmode really necessary

--- helpers/image_helper.rb
+++ helpers/image_helper.rb
@@ -7,10 +7,9 @@ require_relative '../config/StaticData'
 # Class for working with images after convert service
 class ImageHelper
   def self.get_image_size(url)
-    Tempfile.create('images-', StaticData::TMP_FOLDER.to_s) do |fo|
+    Tempfile.create('images-') do |fo|
       fo.binmode
       fo.write(URI.parse(url).open(&:read))
-      fo.rewind
       File.size(fo.path)
     end
   end
