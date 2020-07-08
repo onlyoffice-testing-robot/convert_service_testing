de0c3fa5da3824b63851fccb46f18468f1e0c7ef
Sorokin_Max <askonev007@gmail.com>
2020-07-08 18:45:27 +0300

    Use permalink for images

--- units/test_helpers/test_image_helper_spec.rb
+++ units/test_helpers/test_image_helper_spec.rb
@@ -4,12 +4,12 @@ require '../spec_helper'
 
 describe ImageHelper do
   it 'add a file smaller than 10 KB' do
-    url_image = 'https://raw.githubusercontent.com/onlyoffice-testing-robot/convert_service_testing/feature/unit_test_image_helper/assets/4637.png'
+    url_image = 'https://user-images.githubusercontent.com/60688343/86939678-8c12bb80-c14a-11ea-97d4-e07c6e779b8a.png'
     expect(ImageHelper.get_image_size(url_image.to_s)).to eq(4637)
   end
 
   it 'add a file larger than 10 KB' do
-    url_image = 'https://raw.githubusercontent.com/onlyoffice-testing-robot/convert_service_testing/feature/unit_test_image_helper/assets/17129.png'
+    url_image = 'https://user-images.githubusercontent.com/60688343/86939699-959c2380-c14a-11ea-8d31-337163799021.png'
     expect(ImageHelper.get_image_size(url_image.to_s)).to eq(17_129)
   end
 end
